59ffee7552401658cd5657412c48aff5f81ba3a4
annagao <ggaoyanyan@gmail.com>
2021-09-14 16:28:17 -0700

    add downlaoding button

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,25 @@ const navToggleBar = () => {
     });
 };
 navToggleBar();
+
+/*
+
+(function type() {
+    const texts = ["welcome", "to", "my", "website"];
+    let count = 0;
+    let index = 0;
+    let currentText = "";
+    let letter = "";
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 400);
+})();*/
