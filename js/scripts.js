8eb3d38c6f36b97e46ab2d5235dcc8cfad785a67
annagao <ggaoyanyan@gmail.com>
2021-09-18 00:13:32 -0700

    modify navbar third times

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-$(document).ready(function() {
-    $(".nav-item").click(function() {
-        $("#navbarResponsive").hide();
+$(window).on('load', function() {
+    $('.skill_value').each(function(index, el) {
+        thisVal = $(this).text();
+        $(this).parent('.skill_view').animate({ width: thisVal }, 2000)
     });
-    $(".openButton").click(function() {
-        $("#navbarResponsive").show();
-    });
+
 });
 
 
@@ -12,14 +11,13 @@ const navToggleBar = () => {
     const burger = document.querySelector('.navbar-toggler');
     const navItems = document.querySelectorAll('.nav-item');
     const navLinks = document.querySelectorAll('.nav-link');
-    const navBar = document.querySelector('#navbarResponsive');
     burger.addEventListener('click', () => {
         //animate links
         navItems.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = " ";
             } else {
-                link.style.animation = `navItemFade 0.5s ease forwards ${index /7 +0.2}s`
+                link.style.animation = `navItemFade 0.5s ease forwards ${index /7 }s`
             }
         });
 
@@ -28,9 +26,19 @@ const navToggleBar = () => {
 };
 navToggleBar();
 
-$(window).on('load', function() {
-    $('.skill_value').each(function(index, el) {
-        thisVal = $(this).text();
-        $(this).parent('.skill_view').animate({ width: thisVal }, 2000)
+$(document).ready(function() {
+    $(".nav-item").click(function() {
+        $("#navbarResponsive").hide();
+        $("#openButton").show();
+    });
+    $("#openButton").click(function() {
+        $("#navbarResponsive").show();
+        $("#openButton").hide();
+        $("#closeButton").show();
+    });
+    $("#closeButton").click(function() {
+        $("#navbarResponsive").hide();
+        $("#closeButton").hide();
+        $("#openButton").show();
     });
 });
