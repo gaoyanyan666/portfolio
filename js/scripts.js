2843b3d77f511dd8802fcabf35e9167c6f6340ce
annagao <ggaoyanyan@gmail.com>
2021-09-21 21:52:55 -0700

    modify navbar and portfolio page

--- js/scripts.js
+++ js/scripts.js
@@ -3,42 +3,27 @@ $(window).on('load', function() {
         thisVal = $(this).text();
         $(this).parent('.skill_view').animate({ width: thisVal }, 2000)
     });
-
 });
 
+
+$(document).ready(function() {
+    $(".nav-link").on('click', function() {
+        $('.navbar-collapse').collapse('hide');
+    })
+});
 
 const navToggleBar = () => {
     const burger = document.querySelector('.navbar-toggler');
     const navItems = document.querySelectorAll('.nav-item');
     const navLinks = document.querySelectorAll('.nav-link');
-    burger.addEventListener('click', () => {
-        //animate links
-        navItems.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = " ";
-            } else {
-                link.style.animation = `navItemFade 0.5s ease forwards ${index /7 }s`
-            }
-        });
-
+    const activeNavItem = document.querySelector(".nav-item.active");
+    const navBar = document.querySelector('#navbarResponsive')
+    navItems.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = " ";
+        } else {
+            link.style.animation = `navItemFade 0.5s ease forwards ${index /7 }s`
+        }
     });
-
-};
+}
 navToggleBar();
-
-$(document).ready(function() {
-    $(".nav-item").click(function() {
-        $("#navbarResponsive").hide();
-        $("#openButton").show();
-    });
-    $("#openButton").click(function() {
-        $("#navbarResponsive").show();
-        $("#openButton").hide();
-        $("#closeButton").show();
-    });
-    $("#closeButton").click(function() {
-        $("#navbarResponsive").hide();
-        $("#closeButton").hide();
-        $("#openButton").show();
-    });
-});
